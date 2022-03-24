152429fdc3e12b4badc9edaabce5933ba3b5ad01
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-24 15:28:16 +0800

    Add function operate

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ function add(a, b) {
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
+function operate(operate, num1, num2) {
+  switch (operate) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
