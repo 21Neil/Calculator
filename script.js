e46f56fe9c10bfe203752b5576914abc312c0c6a
Neil <88711171+21Neil@users.noreply.github.com>
2022-03-29 16:52:49 +0800

    Add function operaorBtnOnclick getAnswer

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const buttons = document.querySelectorAll('button')
-const numBtn = document.querySelectorAll('.num')
-const operatorBtn = document.querySelectorAll('.operate')
-const clearBtn = document.querySelector('.clear')
-const backBtn = document.querySelector('.back')
-const equalBtn = document.querySelector('.equal')
-const view = document.querySelector('.view')
-let num = '';
+const buttons = document.querySelectorAll('button');
+const numBtn = document.querySelectorAll('.num');
+const operatorBtn = document.querySelectorAll('.operate');
+const clearBtn = document.querySelector('.clear');
+const backBtn = document.querySelector('.back');
+const equalBtn = document.querySelector('.equal');
+const view = document.querySelector('.view');
+let num1 = '';
 let operator = '';
+let num2 = '';
 
 function add(a, b) {
   return a + b;
@@ -28,63 +29,89 @@ function operate(operate, num1, num2) {
   switch (operate) {
     case '+':
       return add(num1, num2);
-    case '-':
+    case '−':
       return subtract(num1, num2);
-    case '*':
+    case '×':
       return multiply(num1, num2);
-    case '/':
+    case '÷':
       return divide(num1, num2);
   }
 }
 
 //----------handle button style----------
 buttons.forEach(btn => {
-  btn.addEventListener('mouseenter', mouseenter)
-  btn.addEventListener('mouseleave', mouseleave)
-  btn.addEventListener('mousedown', mouseDown)
-  btn.addEventListener('mouseup', mouseUp)
-})
+  btn.addEventListener('mouseenter', mouseenter);
+  btn.addEventListener('mouseleave', mouseleave);
+  btn.addEventListener('mousedown', mouseDown);
+  btn.addEventListener('mouseup', mouseUp);
+});
 
 function mouseenter(e) {
-  e.target.classList.add('enter')
+  e.target.classList.add('enter');
 }
 
 function mouseleave(e) {
-  e.target.classList.remove('enter')
-  e.target.classList.remove('click')
+  e.target.classList.remove('enter');
+  e.target.classList.remove('click');
 }
 
 function mouseDown(e) {
-  e.target.classList.add('click')
+  e.target.classList.add('click');
 }
 
 function mouseUp(e) {
-  e.target.classList.remove('click')
+  e.target.classList.remove('click');
 }
 //----------handle button style----------
 
 numBtn.forEach(btn => {
-  btn.addEventListener('click', numOnclick)
-})
+  btn.addEventListener('click', numOnclick);
+});
 
 function numOnclick(e) {
-  if(num.length > 14) return
-  view.textContent += e.target.value
-  num += e.target.value;
+  if (num1.length > 14) return;
+  num1 += e.target.value;
+  view.textContent = num1;
 }
 
-clearBtn.addEventListener('click', clear)
+clearBtn.addEventListener('click', clear);
 
 function clear() {
-  view.textContent = ''
-  num = '';
+  view.textContent = '';
+  num1 = '';
+  num2 = '';
 }
 
 backBtn.addEventListener('click', back);
 
 function back() {
-  view.textContent = view.textContent.slice(0, view.textContent.length - 1)
-  num = num.slice(0, num.length - 1);
+  view.textContent = view.textContent.slice(0, view.textContent.length - 1);
+  num1 = num1.slice(0, num1.length - 1);
 }
 
-equalBtn.addEventListener('click', operate)
+operatorBtn.forEach(btn => {
+  btn.addEventListener('click', operatorBtnOnclick);
+});
+
+function operatorBtnOnclick(e) {
+  if (num2 === '') {
+    num2 = num1;
+    num1 = '';
+    view.textContent = num2;
+  } else {
+    let answer = operate(operator, +num2, +num1);
+    view.textContent = answer;
+    num1 = ''
+    num2 = answer
+  }
+  operator = e.target.value;
+}
+
+equalBtn.addEventListener('click', getAnswer);
+
+function getAnswer() {
+  let answer = operate(operator, +num2, +num1);
+  view.textContent = answer;
+  num1 = answer
+  num2 = ''
+}
